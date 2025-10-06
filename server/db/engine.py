819bd95a8414a26a211fb01e3f7d85b3b1b28733
kraigochieng <kraigochieng@gmail.com>
2025-10-06 23:40:19 +0300

    moved these files into src folder

--- server/db/engine.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine
-from . import DB_PATH
-
-engine = create_engine(f"sqlite:///{DB_PATH}")
--- server/ml/artifacts/__init__.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-ARTIFACTS_DIR = Path(__file__).resolve().parent
-
-# These paths rely on what is in MLFlow, change accordingly if required
-ML_MODEL_PATH = f"{ARTIFACTS_DIR}/model/model.pkl"
-SCALERS_PATH = f"{ARTIFACTS_DIR}/scalers/minmax_scaler.pkl"
-ENCODERS_PATH = f"{ARTIFACTS_DIR}/encoders"
-METADATA_PATH = f"{ARTIFACTS_DIR}/metadata/model_columns.json"
