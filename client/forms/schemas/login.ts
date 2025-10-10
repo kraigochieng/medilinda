7ffcec2a75c89b3e707b1f706e7be8c2f7e99244
kraigochieng <kraigochieng@gmail.com>
2025-10-11 00:01:40 +0300

    removed forms used with veevalidate

--- client/forms/schemas/login.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-
-export const loginValidationSchema = z.object({
-	username: z.string(),
-	password: z.string(),
-});
-
-export type loginTypeValidationSchema = z.infer<typeof loginValidationSchema>
