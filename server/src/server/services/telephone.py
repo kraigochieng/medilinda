75c6c4e9bc3aa6660871c0e5e49a7085651c259c
kraigochieng <kraigochieng@gmail.com>
2025-10-25 02:10:33 +0300

    added telephone service

---
+++ server/src/server/services/telephone.py
@@ -0,0 +1,31 @@
+from fastapi_pagination import Page
+
+from server.basemodels.medical_institution import (
+    MedicalInstitutionGetResponse,
+    MedicalInstitutionTelephonePostRequest,
+)
+from server.repositories.telephone import TelephoneRepository
+
+
+class TelephoneService:
+    def __init__(self, db):
+        self.repository = TelephoneRepository(db)
+
+    def get_telephones(self) -> Page[MedicalInstitutionGetResponse]:
+        return self.repository.get_all()
+
+    def get_telephone_by_id(self, telephone_id: str) -> MedicalInstitutionGetResponse:
+        return self.repository.get_by_id(telephone_id)
+
+    def create_telephone(
+        self, telephone: MedicalInstitutionTelephonePostRequest
+    ) -> MedicalInstitutionGetResponse:
+        return self.repository.create(telephone)
+
+    def update_telephone(
+        self, telephone_id: str, telephone: MedicalInstitutionTelephonePostRequest
+    ) -> MedicalInstitutionGetResponse:
+        return self.repository.update(telephone_id, telephone)
+
+    def delete_telephone(self, telephone_id: str) -> bool:
+        return self.repository.delete(telephone_id)
