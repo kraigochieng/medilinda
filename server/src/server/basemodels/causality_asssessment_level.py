cc28b86909c8214f1ce73dbdcc2f4e38c123e796
kraigochieng <kraigochieng@gmail.com>
2025-10-26 12:53:54 +0300

    added causality assessment level post request basemodel

--- server/src/server/basemodels/causality_asssessment_level.py
+++ server/src/server/basemodels/causality_asssessment_level.py
@@ -12,11 +12,25 @@ class CausalityAssessmentLevelEnum(str, enum.Enum):
     unclassified = "unclassified"
     unclassifiable = "unclassifiable"
 
+
 class CausalityAssessmentLevelGetResponse(BaseModel):
     id: str
     adr_id: str
     ml_model_id: str = "final_ml_model@champion"
     causality_assessment_level_value: CausalityAssessmentLevelEnum
+
+    base_values: Optional[List[float]] = None
+    shap_values_matrix: Optional[List[List[float]]] = None
+    shap_values_sum_per_class: Optional[List[float]] = None
+    shap_values_and_base_values_sum_per_class: Optional[List[float]] = None
+    feature_names: Optional[List[str]] = None
+    feature_values: Optional[List[Any]] = None
+
+
+class CausalityAssessmentLevelPostRequest(BaseModel):
+    adr_id: str | None
+    ml_model_id: str = "final_ml_model@champion"
+    causality_assessment_level_value: CausalityAssessmentLevelEnum | None
 
     base_values: Optional[List[float]] = None
     shap_values_matrix: Optional[List[List[float]]] = None
