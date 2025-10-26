ced922e04082d1240cdedb2d6eca9407505625ef
kraigochieng <kraigochieng@gmail.com>
2025-10-26 12:50:45 +0300

    added services

---
+++ server/src/server/services/causality_assessment_level.py
@@ -0,0 +1,58 @@
+from fastapi_pagination import Page
+
+from server.basemodels.causality_asssessment_level import (
+    CausalityAssessmentLevelGetResponse,
+    CausalityAssessmentLevelPostRequest,
+)
+from server.basemodels.review import ReviewPostRequest, ReviewGetResponse
+from server.repositories.causality_assessment_level import (
+    CausalityAssessmentLevelRepository,
+)
+from server.repositories.review import ReviewRepository
+from server.repositories.user import UserRepository
+
+
+class CausalityAssessmentLevelService:
+    def __init__(self, db):
+        self.repo = CausalityAssessmentLevelRepository(db)
+        self.review_repo = ReviewRepository(db)
+        self.user_repo = UserRepository(db)
+
+    def get_causality_assessment_level_by_id(
+        self, cal_id: str
+    ) -> CausalityAssessmentLevelGetResponse | None:
+        return self.repo.get_by_id(cal_id)
+
+    def get_causality_assessment_levels(
+        self,
+    ) -> Page[CausalityAssessmentLevelGetResponse]:
+        return self.repo.get_all()
+
+    def update_causality_assessment_level_by_id(
+        self, id: str, data: CausalityAssessmentLevelPostRequest
+    ) -> CausalityAssessmentLevelGetResponse | None:
+        return self.repo.update(id, data)
+
+    def delete_causality_assessment_level_by_id(
+        self, id: str
+    ) -> CausalityAssessmentLevelGetResponse | None:
+        return self.repo.delete(id)
+
+    def create_review(
+        self, id: str, username: str, data: ReviewPostRequest
+    ) -> ReviewGetResponse | None:
+        cal_model = self.repo.get_by_id(id)
+
+        if not cal_model:
+            return None
+
+        user = self.user_repo.get_by_username(username=username)
+
+        if not user:
+            return None
+
+        review_model = self.review_repo.create(
+            data=data, causality_assessment_level_id=id, user_id=user.id
+        )
+
+        return review_model
--- server/src/server/services/review.py
+++ server/src/server/services/review.py
@@ -1,37 +1,35 @@
-from sqlalchemy.orm import Session
-from server.repositories.review import ReviewRepository
-from server.basemodels.review import (
-    ADRReviewCreateRequest,
-    ADRReviewGetResponse,
-    ReviewGetResponse,
-)
 from fastapi_pagination import Page
+from sqlalchemy.orm import Session
+
+from server.basemodels.review import (
+    ReviewGetResponse,
+    ReviewPostRequest,
+)
+from server.repositories.review import ReviewRepository
 
 
 class ReviewService:
     def __init__(self, db: Session):
         self.repository = ReviewRepository(db)
 
-    def get_reviews(self) -> Page[ReviewGetResponse]:
-        return self.repository.get_all()
+    def get_reviews(
+        self, causality_assessment_level_id: str | None
+    ) -> Page[ReviewGetResponse]:
+        return self.repository.get_all(
+            causality_assessment_level_id=causality_assessment_level_id
+        )
 
     def get_review_by_id(self, review_id: str) -> ReviewGetResponse:
         return self.repository.get(review_id)
 
     def create_review(
         self,
-        review_data: ADRReviewCreateRequest,
-        causality_assessment_level_id: str,
-        user_id: str,
+        data: ReviewPostRequest,
     ) -> ReviewGetResponse:
-        return self.repository.create(
-            review_data, causality_assessment_level_id, user_id
-        )
+        return self.repository.create(data=data)
 
-    def update_review(
-        self, review_id: str, review_update: ADRReviewCreateRequest
-    ) -> ReviewGetResponse:
-        return self.repository.update(review_id, review_update)
+    def update_review(self, id: str, data: ReviewPostRequest) -> ReviewGetResponse:
+        return self.repository.update(id, data)
 
-    def delete_review(self, review_id: str) -> bool:
-        return self.repository.delete(review_id)
+    def delete_review(self, id: str) -> bool:
+        return self.repository.delete(id)
