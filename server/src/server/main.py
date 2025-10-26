c2e2c1c484ffd3015f74e1de36cc568c86f9c608
kraigochieng <kraigochieng@gmail.com>
2025-10-26 17:05:27 +0300

    removed routers for unnecessary endpoints

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -17,13 +17,9 @@ from server.api.v1.endpoints import (
 from server.api.v1.endpoints import (
     causality_assessment_levels_actions as causality_assessment_levels_actions_v1,
 )
-from server.api.v1.endpoints import (
-    causality_assessment_levels_details as causality_assessment_levels_details_v1,
-)
 from server.api.v1.endpoints import dashboard as dashboard_v1
 from server.api.v1.endpoints import medical_institution as medical_institution_v1
 from server.api.v1.endpoints import reviews as reviews_v1
-from server.api.v1.endpoints import reviews_details as reviews_details_v1
 from server.api.v1.endpoints import sms as sms_v1
 from server.api.v1.endpoints import sms_actions as sms_actions_v1
 from server.api.v1.endpoints import sms_details as sms_details_v1
@@ -52,11 +48,9 @@ app.include_router(adverse_drug_reaction_report_v1.router)
 app.include_router(adverse_drug_reaction_report_details_v1.router)
 app.include_router(causality_assessment_level_v1.router)
 app.include_router(causality_assessment_levels_actions_v1.router)
-app.include_router(causality_assessment_levels_details_v1.router)
 app.include_router(dashboard_v1.router)
 app.include_router(medical_institution_v1.router)
 app.include_router(reviews_v1.router)
-app.include_router(reviews_details_v1.router)
 app.include_router(sms_v1.router)
 app.include_router(sms_actions_v1.router)
 app.include_router(sms_details_v1.router)
