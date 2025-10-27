72a324837ea50cd0338aeafe6067b3818b2b5bcb
kraigochieng <kraigochieng@gmail.com>
2025-10-27 10:50:12 +0300

    removed endpoint for cal actions

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -14,9 +14,6 @@ from server.api.v1.endpoints import auth as auth_v1
 from server.api.v1.endpoints import (
     causality_assessment_levels as causality_assessment_level_v1,
 )
-from server.api.v1.endpoints import (
-    causality_assessment_levels_actions as causality_assessment_levels_actions_v1,
-)
 from server.api.v1.endpoints import dashboard as dashboard_v1
 from server.api.v1.endpoints import medical_institution as medical_institution_v1
 from server.api.v1.endpoints import reviews as reviews_v1
@@ -47,7 +44,6 @@ app.include_router(auth_v1.router)
 app.include_router(adverse_drug_reaction_report_v1.router)
 app.include_router(adverse_drug_reaction_report_details_v1.router)
 app.include_router(causality_assessment_level_v1.router)
-app.include_router(causality_assessment_levels_actions_v1.router)
 app.include_router(dashboard_v1.router)
 app.include_router(medical_institution_v1.router)
 app.include_router(reviews_v1.router)
