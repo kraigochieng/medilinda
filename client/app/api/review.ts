ddcda427753293ef48fe796165f21fcbb205e48a
kraigochieng <kraigochieng@gmail.com>
2025-11-04 02:02:30 +0300

    added apis for client mutations

--- client/app/api/review.ts
+++ client/app/api/review.ts
@@ -1,17 +1,37 @@
-import type { ReviewGetResponse } from "~/types/review";
+import type { PaginatedResponseInterface } from "~/types/pagination";
+import type { ReviewGetResponse, ReviewStatsGetResponse } from "~/types/review";
 
-const path = "reviews-details";
+const path = "reviews";
+const statsPath = "reviews-details";
 
-export async function fetchReviewByUserAndCausalityLevel(
-	causalityAssessmentLevelId: string
-): Promise<ReviewGetResponse> {
+export async function fetchReviews(params: {
+	page?: number;
+	size?: number;
+	causality_assessment_level_id?: string;
+	user_id?: string;
+}): Promise<PaginatedResponseInterface<ReviewGetResponse>> {
 	const { $serverFetch } = useNuxtApp();
 
-	return await $serverFetch<ReviewGetResponse>(
-		`/${path}/specific-user-and-causality-assessment-level`,
+	return await $serverFetch<PaginatedResponseInterface<ReviewGetResponse>>(
+		`/${path}/`,
 		{
 			method: "GET",
-			query: { causality_assessment_level_id: causalityAssessmentLevelId },
+			query: params,
+		}
+	);
+}
+
+export async function fetchReviewStats(
+	causality_assessment_level_id: string
+): Promise<ReviewStatsGetResponse> {
+	const { $serverFetch } = useNuxtApp();
+
+	// Calls: GET /api/v1/reviews-details/{id}/stats
+	return await $serverFetch<ReviewStatsGetResponse>(
+		`/${statsPath}/${causality_assessment_level_id}/stats`,
+		{
+			method: "GET",
+			// No query params are needed, as the ID is in the path
 		}
 	);
 }
