8c02736072fbb8076c49f0d95397528dd392c98b
kraigochieng <kraigochieng@gmail.com>
2025-11-04 02:06:13 +0300

    updated composables for shap components

--- client/app/composables/useClassLabelFromNumber.ts
+++ client/app/composables/useClassLabelFromNumber.ts
@@ -1,16 +1,21 @@
-export function useClassLabelFromNumber(classNumber: number): string | undefined {
-	switch (classNumber) {
-		case 0:
-			return "certain";
-		case 1:
-			return "likely";
-		case 2:
-			return "possible";
-		case 3:
-			return "unlikely";
-		case 4:
-			return "unclassified";
-		case 5:
-			return "unclassifiable";
-	}
-}
+// export function useClassLabelFromNumber(classNumber: number): string | undefined {
+// 	switch (classNumber) {
+// 		case 0:
+// 			return "certain";
+// 		case 1:
+// 			return "likely";
+// 		case 2:
+// 			return "possible";
+// 		case 3:
+// 			return "unlikely";
+// 		case 4:
+// 			return "unclassified";
+// 		case 5:
+// 			return "unclassifiable";
+// 	}
+// }
+
+export const useClassLabelFromNumber = (index: number): string => {
+	const labels = ["certain", "likely", "possible", "unlikely"];
+	return labels[index] || "unknown";
+};
