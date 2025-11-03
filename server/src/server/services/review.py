7fae3187ebbdd3190312e7733cb01610623036d5
kraigochieng <kraigochieng@gmail.com>
2025-11-04 02:17:32 +0300

    updated review module

--- server/src/server/services/review.py
+++ server/src/server/services/review.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from server.basemodels.review import (
     ReviewGetResponse,
     ReviewPostRequest,
+    ReviewStatsResponse,
 )
 from server.repositories.review import ReviewRepository
 
@@ -21,6 +22,26 @@ class ReviewService:
 
     def get_review_by_id(self, review_id: str) -> ReviewGetResponse:
         return self.repository.get(review_id)
+
+    def get_review_stats(
+        self, causality_assessment_level_id: str
+    ) -> ReviewStatsResponse:
+        """
+        Retrieves review counts and maps them to the response model.
+        """
+        # Call the repository to get the database row
+        counts_row = self.repository.get_review_counts_by_causality_level(
+            causality_assessment_level_id=causality_assessment_level_id
+        )
+
+        # Map the RowProxy (which acts like a dict) to the Pydantic model
+        # Because we set orm_mode = True in the BaseModel,
+        # we can use .from_orm() for clean mapping.
+        if counts_row:
+            return ReviewStatsResponse.model_validate(counts_row)
+
+        # Fallback in case something goes wrong, though count() should always return a row
+        return ReviewStatsResponse(approved_reviews=0, unapproved_reviews=0)
 
     def create_review(
         self,
